873da3a50881ec03077968cdfdbe0c8f73ff36ae
Drew McDonald <drew.mcdonald@secondstreet.com>
2020-02-24 09:25:35 -0600

    2.1.0

---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Add Script To Post ===
+Contributors: Second Street (Heather McCarron)
+Tags: Page, Post, Shortcode, Promotion, Second Street, UPICKEM
+Requires at least: 3.0
+Tested up to: 3.9.2
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Embed your Second Street Promotion on a WordPress page using the Shortcode generated in the Second Street Partner Tool.
+
+
+== Description ==
+
+The Second Street Promotion Plugin will allow you to embed your Second Street Promotion within your WordPress site. The feature to generate the Shortcode required for this plugin is located within your promotion's [Second Street Partner Tool](https://affiliates.upickem.net). After retrieving your Promotion's Shortcode, you will paste it into the page content area of the WordPress page you would like it to appear on.
+
+NOTE: The embedded Promotion's width will be dependent on the size of the parent container/element that it is placed in. If your promotion appears too narrow or too wide, try adjusting the width of its container.
+
+
+== Installation ==
+
+1. Extract files from the ZIP file.
+2. Upload `secondstreet-promotion` folder (contains `secondstreet-promotion.php`) 
+   to the `/wp-content/plugins/` directory.
+3. Activate the plugin through the 'Plugins' menu in WordPress.
+4. Retrieve (copy) your Promotion's Shortcode from your Second Street Partner Tool. 
+   (Login to your Second Street account at https://affiliates.upickem.net) 
+5. Place (paste) your Shortcode into the page content area in your WordPress site. 
+
+
+== Frequently Asked Questions ==
+
+= I cannot activate the Second Street Plugin. What should I do? =
+
+If you are having problems activating your plugin, please contact Second Street's [Support Department](http://affiliatesupport@secondstreet.com) to ask for help.
+
+
+== Changelog == 
+
+= 2.1.0 =
+* Support for ss-promo, ss-signup, and ss-contest shortcodes
---
+++ secondstreet-promotion.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Plugin Name: Second Street * Description: Plugin will allow Second Street Affiliates to embed a Second Street Promotion within their WordPress site(s).
+ * Version: 2.1
+ * Author: Second Street
+ * Author URI: http://secondstreet.com
+ * License: GPL2
+ */
+
+/*  Copyright 2014  Second Street (email : wordpress@secondstreet.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+**************************************************************************/
+
+// Blocks direct access to plugin
+defined( 'ABSPATH' ) or die( "Access Forbidden" );
+
+defined( 'ABSPATH' ) or die( "Access Forbidden" ); // Blocks direct access to plugin
+
+// Define Second Street Plugin
+define( 'SECONDSTREET_PLUGIN_VERSION', '1.0' );
+define( 'SECONDSTREET_PLUGIN__MINIMUM_WP_VERSION', '3.1' );
+define( 'SECONDSTREET_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'SECONDSTREET_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+
+// [ss-promo] Code
+function ss_promo_func( $atts, $content = null ) {
+	$a = shortcode_atts( array (
+			'op_id' => '',
+			'op_guid' => '',
+			'routing' => ''
+		), $atts );
+
+	$ss_script_url = 'https://embed-' . $a['op_id'] . '.secondstreetapp.com/Scripts/dist/embed.js';
+
+	return '<script src="' . esc_url( $ss_script_url ) . '" data-ss-embed="promotion" data-opguid="' . esc_attr( $a['op_guid'] ) . '" data-routing="' . esc_attr( $a['routing'] ) . '"></script>';
+
+}
+add_shortcode( 'ss-promo', 'ss_promo_func' );
+
+// [ss-signup] Code
+function ss_signup_func( $atts, $content = null ) {
+	$a = shortcode_atts( array (
+			'design_id' => ''
+		), $atts );
+
+	$ss_script_url = 'https://embed.secondstreetapp.com/Scripts/dist/optin.js';
+
+	return '<script src="' . esc_url( $ss_script_url ) . '" data-ss-embed="optin" data-design-id="' . esc_attr( $a['design_id'] ) . '"></script>';
+
+}
+
+add_shortcode( 'ss-promo', 'ss_promo_func' );
+add_shortcode( 'ss-signup', 'ss_signup_func' );
+
+
+// [ss-contest] Code
+function ss_contest_func( $atts, $content = null ) {
+	$a = shortcode_atts( array (
+			'contest_url' => '',
+			'contest_folder' => '',
+			'routing' => ''
+	), $atts );
+
+	$ss_script_url =  $a['contest_url'] . '/' . 'shared/embedcode/embed.js';
+
+	return '<script type="text/javascript" src="' . esc_attr( $a['contest_url'] ) . '/shared/embedcode/talker-v1.0.0.js"></script><script src="' . esc_url( $ss_script_url ) . '" data-ss-embed="contest" data-routing="' . esc_attr( $a['routing'] ) . '"></script>';
+
+}
+
+add_shortcode( 'ss-promo', 'ss_promo_func' );
+add_shortcode( 'ss-signup', 'ss_signup_func' );
+
+add_shortcode( 'ss-contest', 'ss_contest_func' );
