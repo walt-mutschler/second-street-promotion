373a036a444f0dcdf2c39b984a277cb1d163eb79
Vladic Kostin <vladic.kostin@secondstreet.com>
2020-03-19 13:48:31 -0500

    Bump to 3.1.1

--- secondstreet-promotion.php
+++ secondstreet-promotion.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Second Street
  * Description: Plugin will allow Second Street Affiliates to embed a Second Street Promotion within their WordPress site(s).
- * Version: 3.1.0
+ * Version: 3.1.1
  * Author: Second Street
  * Author URI: http://secondstreet.com
  * License: GPL2
